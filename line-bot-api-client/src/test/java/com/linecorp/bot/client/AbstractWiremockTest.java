89dca0b82ab0fe1de5f3fa9bf32e3d6693d6f006
Narison <30853865+Narison@users.noreply.github.com>
2017-08-24 15:10:33 +0700

    Update AbstractWiremockTest.java

--- line-bot-api-client/src/test/java/com/linecorp/bot/client/AbstractWiremockTest.java
+++ line-bot-api-client/src/test/java/com/linecorp/bot/client/AbstractWiremockTest.java
@@ -62,7 +62,7 @@ public abstract class AbstractWiremockTest {
 
     protected LineMessagingClientImpl createLineMessagingClient(final MockWebServer mockWebServer) {
         LineMessagingService lineMessagingService =
-                LineMessagingServiceBuilder.create("token")
+                LineMessagingServiceBuilder.create("HnYiosVXrVsSbH35XHeQCkTgPn6Pa3shrsh+K7RJ8RIPF8hpXr4IBm40k/4B2lmr1mIRRl+JFwcohXq7JIXSmOOoBl2UhUoaMEGuRaD5uQ1kXURzsh2vwjY58D1/RPO523ZweZArgtN8XaHy5eZJvQdB04t89/1O/w1cDnyilFU=")
                                            .apiEndPoint("http://localhost:" + mockWebServer.getPort())
                                            .build();
         return new LineMessagingClientImpl(lineMessagingService);
